6e3d9ee39b87720b86b25f57e0cf8b7c6e27fafd
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2024-12-12 19:29:42 -0300

    feat: add initial Snake game setup

--- main.py
+++ main.py
@@ -5,3 +5,26 @@ import pygame
 import time
 import random
 
+# Definição da velocidade da cobra
+snake_speed = 15
+
+# Tamanho da janela
+window_x = 720
+window_y = 480
+
+# Definindo cores 
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+
+# Inicializando o pygame
+pygame.init()
+
+# Inicializando a janela do jogo
+pygame.display.set_caption('GeeksforGeeks Snakes')
+game_window = pygame.display.set_mode((window_x, window_y))
+
+# Controlador de FPS (frames por segundo)
+fps = pygame.time.Clock()
